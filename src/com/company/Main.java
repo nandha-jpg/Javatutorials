ea3c6f08b2efbd51507ca47a6f7c03da62ce61a0
Nandha Kumar <48424140+nandha-jpg@users.noreply.github.com>
2021-05-12 15:02:27 +0530

    reversed the column flow

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         for (int row = 1; row <= 3; row++) {
-            for (int column=1; column<=3; column++)
+            for (int column=3; column>=1; column--)
             {
                 System.out.println("the column value is "+column);
             }
 
-            System.out.println("the value of row is " + row);
+            System.out.println(/*"the value of row is " + row*/);
         }
     }
 }
