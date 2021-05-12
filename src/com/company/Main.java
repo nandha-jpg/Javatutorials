14d74dec5a6de178fb6659e4c4ae874e9ea6f766
Nandha Kumar <48424140+nandha-jpg@users.noreply.github.com>
2021-05-12 14:36:02 +0530

    placed for loop inside the for loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         for (int row = 1; row <= 3; row++) {
+            for (int column=1; column<=3; column++)
+            {
+                System.out.println("the column value is "+column);
+            }
 
             System.out.println("the value of row is " + row);
         }
